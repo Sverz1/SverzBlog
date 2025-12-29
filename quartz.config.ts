e4f14480875a076d1ba0059ba9bcfa012d69fb7e
Sverz1 <fabio.campitelli@live.it>
2025-12-29 20:07:48 +0100

    Quartz sync: Dec 29, 2025, 8:07 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "SverzBlog",
     pageTitleSuffix: "",
     branch: 'main',
     enableSPA: true,
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "sverz1.github.io/SverzBlog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
